84eeb3d4791790326666288503cc0394b5bcc142
Robert Martin del Campo <robert.delcampo@mindera.com>
2019-05-18 09:37:14 -0700

    chore(): Updated storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,20 +1,9 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-// const req = require.context('../stories', true, /\.stories\.js$/);
-// const req = require.context('../src/*', true, /\.stories\.js$/);
-// const req = require.context('../src/atomic.ui/*.*/*', true, /\.stories\.js$/);
+const req = require.context("../src/atomic.ui", true, /\.story\.jsx?$/);
+
 function loadStories() {
-  // req.keys().forEach(filename => req(filename));
-  require('../src/atomic.ui/atoms/Button/index.story');
-  require('../src/atomic.ui/atoms/Input/index.story');
-  require('../src/atomic.ui/molecules/Select/index.story');
-  require('../src/atomic.ui/atoms/Checkbox/index.story');
-  require('../src/atomic.ui/atoms/Label/index.story');
-  require('../src/atomic.ui/molecules/Task/index.story');
-  require('../src/atomic.ui/molecules/TaskPriority/index.story');
-  require('../src/atomic.ui/molecules/TaskType/index.story');
-  require('../src/atomic.ui/organisms/TaskForm/index.story');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
